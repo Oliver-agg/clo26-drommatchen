0d23dc38ac6482decf09a313e92ac0e7b4222456
Oliver-agg <ad07720@skola.goteborg.se>
2026-09-16 19:27:14 +0200

    Implement Match class

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -1,43 +1,29 @@
 public class Match
 {
-    // Privata fält
-    private string _hemmalag;
-    private string _bortalag;
-    private string _datum;
 
-    // Properties — publik get, privat set
-    public string Hemmalag
-    {
-        // get och privat set för Hemmalag 
-    }
+    
+    public string Hemmalag { get; private set; }
 
-    public string Bortalag
-    {
-        // get och privat set för Bortalag
-    }
-
-    public string Datum
-    {
-        // get och privat set för Datum
-    }
-
-    // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
-
-    // Konstruktor
+    public string Bortalag { get; private set; }
+  
+    public string Datum { get; private set; }
+    
     public Match(string hemmalag, string bortalag, string datum)
     {
-        // TODO: tilldela de privata fälten
+        Hemmalag = hemmalag;
+        Bortalag = bortalag;
+        Datum = datum;
     }
 
     // Metod 1: skriv ut matchens lag och datum
     public void Presentera()
     {
-        // TODO
+        Console.WriteLine($"Dagens match är mellan {Hemmalag} och {Bortalag} idag den {Datum}");
     }
 
     // Metod 2: skriv ut spelarens namn, nummer och position
     public void AnnounceraMålskytt(Spelare spelare)
     {
-        // TODO
+        Console.WriteLine($"Idag spelar dessutom {spelare.Namn} med nummer {spelare.Nummer} på position {spelare.Position}");
     }
 }
