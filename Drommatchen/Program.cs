0ae614011ae7421e0e4f25d5ef497774de753133
Oliver-agg <ad07720@skola.goteborg.se>
2026-09-17 14:31:16 +0200

    Added match.presentera, match.announceraMålskytt spelare1 and spelare2

--- Drommatchen/Program.cs
+++ Drommatchen/Program.cs
@@ -1,10 +1,16 @@
 Spelare spelare1 = new Spelare("Lionel Messi", 10, "Winger");
 Spelare spelare2 = new Spelare("Cristiano Ronaldo", 9, "Striker");
-Match match = new Match("Portugal", ")
+Match match = new Match("Portugal", "Argentina", "2026-10-10");
 
-                                                              // TODO: Skapa 1 match
-                                                              // Match match = new Match("Hemmalag", "Bortalag", "Datum");
+match.Presentera();
 
-                                                              // TODO: Anropa match.Presentera()
+match.AnnounceraMålskytt(spelare1);
+match.AnnounceraMålskytt(spelare2);
 
-                                                              // TODO: Anropa match.AnnounceraMålskytt() med båda spelarna
+
+// TODO: Skapa 1 match
+// Match match = new Match("Hemmalag", "Bortalag", "Datum");
+
+// TODO: Anropa match.Presentera()
+
+// TODO: Anropa match.AnnounceraMålskytt() med båda spelarna
